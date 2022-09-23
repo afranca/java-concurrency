8afddb113723d608fa61dfda16a581035af24507
Alexandre Franca <basic4@gmail.com>
2022-09-23 08:16:37 +0100

    feat: creating 5 threads with different priorities

---
+++ src/com/alexandrefranca/starvation/Starvation.java
@@ -0,0 +1,48 @@
+package com.alexandrefranca.starvation;
+
+import com.alexandrefranca.util.ThreadColor;
+
+public class Starvation {
+
+    public static Object lock = new Object();
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Worker(ThreadColor.ANSI_RED), "P10");
+        Thread t2 = new Thread(new Worker(ThreadColor.ANSI_BLUE), "P08");
+        Thread t3 = new Thread(new Worker(ThreadColor.ANSI_GREEN), "P06");
+        Thread t4 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "P04");
+        Thread t5 = new Thread(new Worker(ThreadColor.ANSI_PURPLE), "P02");
+
+        t1.setPriority(10);
+        t2.setPriority(8);
+        t3.setPriority(6);
+        t4.setPriority(4);
+        t5.setPriority(2);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+
+    }
+
+    private static class Worker implements Runnable{
+        String colour;
+        int count;
+        public Worker(String colour) {
+            this.colour = colour;
+        }
+
+        @Override
+        public void run() {
+            for(int i=0; i<100; i++){
+                synchronized (lock){
+                    System.out.format(colour+"%s: runCount = %d\n",
+                            Thread.currentThread().getName(),
+                            count++);
+                }
+            }
+        }
+    }
+}
