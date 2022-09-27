0fc15cea2cdfbe64b2e16b13122e5126b8d7938b
Alexandre Franca <basic4@gmail.com>
2022-09-27 14:57:08 +0100

    feat: preparation for challenge6. Adding status for previous code (challenge 5)

---
+++ src/com/alexandrefranca/challenge6/BankAccount.java
@@ -0,0 +1,65 @@
+package com.alexandrefranca.challenge6;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
+class BankAccount {
+
+    private double balance;
+    private String accountNumber;
+    private Lock lock;
+ 
+    public BankAccount(String accountNumber, double initialBalance, Lock lock) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+        this.lock = lock;
+    }
+ 
+    public void deposit(double amount) {
+        try {
+            if (lock.tryLock(1000, TimeUnit.MILLISECONDS)){
+                try {
+                    balance += amount;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("deposit: Could not get lock");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("deposit: InterruptedException");
+        }
+
+    }
+ 
+    public void withdraw(double amount) {
+        try {
+            if (lock.tryLock(1, TimeUnit.SECONDS)){
+                try {
+                    balance -= amount;
+                } finally {
+                    lock.unlock();
+                }
+
+            } else {
+                System.out.println("withdraw: Could not get lock");
+            }
+
+        } catch (InterruptedException e) {
+            System.out.println("withdraw: InterruptedException");
+        }
+
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void printAccountNumber() {
+        System.out.println("Account Number: "+accountNumber);
+    }
+}
---
+++ src/com/alexandrefranca/challenge6/Challenge6Status.java
@@ -0,0 +1,44 @@
+package com.alexandrefranca.challenge6;
+
+import com.alexandrefranca.ThreadColor;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Challenge6Status {
+
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+
+        BankAccount account = new BankAccount("12345-678", 1000.00, lock);
+
+        new Thread((Runnable) () -> {
+            account.deposit(300);
+            System.out.println(ThreadColor.ANSI_RED+"deposit: 300");
+//            try {
+//                Thread.sleep(1_000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+            account.withdraw(50);
+            System.out.println(ThreadColor.ANSI_RED+"withdraw: 50");
+        }).start();
+
+        new Thread((Runnable) () -> {
+            account.deposit(203.75);
+            System.out.println(ThreadColor.ANSI_YELLOW+"deposit: 203.75");
+            account.withdraw(100);
+            System.out.println(ThreadColor.ANSI_YELLOW+"withdraw: 100");
+        }).start();
+
+        try {
+            Thread.sleep(2_000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(ThreadColor.ANSI_BLUE+"Balance:"+ account.getBalance());
+
+    }
+
+}
