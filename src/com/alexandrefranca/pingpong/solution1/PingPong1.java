d2cf9656f1d39d321781a4ffb22d5cfe22fb6980
Alexandre Franca <basic4@gmail.com>
2022-09-28 15:17:33 +0100

    feat: Using thread in lambda style. Solution by Tugrul

--- src/com/alexandrefranca/pingpong/solution1/PingPong1.java
+++ src/com/alexandrefranca/pingpong/solution1/PingPong1.java
@@ -4,22 +4,55 @@ import com.alexandrefranca.ThreadColor;
 
 public class PingPong1 {
 
-    //public Ball ball = new Ball(Boolean.TRUE);
-    public boolean pongWait=true;
+    public Boolean pongWait = true;
+
+    Runnable pinger = ()->{
+        while(!Thread.currentThread().isInterrupted()) {
+            synchronized (this) {
+                while (!pongWait) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                System.out.println(ThreadColor.ANSI_YELLOW+"Ping");
+                pongWait = false;
+                notifyAll();
+            }
+        }
+    };
+
+    Runnable ponger = ()->{
+        while(!Thread.currentThread().isInterrupted()) {
+            synchronized (this) {
+                while (pongWait) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                System.out.println(ThreadColor.ANSI_PURPLE+"Pong");
+                pongWait = true;
+                notifyAll();
+            }
+        }
+    };
 
     public static void main(String[] args) throws InterruptedException{
-        PingPong1 main = new PingPong1();
 
+        PingPong1 main = new PingPong1();
         System.out.println("Starting Ping Pong");
 
-        Thread pingThread = new Thread(new Pinger(main.pongWait, ThreadColor.ANSI_YELLOW));
-        Thread pongThread = new Thread(new Ponger(main.pongWait, ThreadColor.ANSI_PURPLE));
+        Thread pingThread = new Thread(main.pinger);
+        Thread pongThread = new Thread(main.ponger);
 
         pingThread.start();
         pongThread.start();
 
-        //stop threads after 5 seconds
-        Thread.sleep(5*1000);
+        //stop threads after 1 seconds
+        Thread.sleep(1*1000);
         pingThread.interrupt();
         pongThread.interrupt();
     }
